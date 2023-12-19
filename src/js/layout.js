3c737ce7fda77e439bc9d86ba7a673fa6cd40bd4
rochelleaguila <143100232+rochelleaguila@users.noreply.github.com>
2023-12-19 22:32:29 +0000

    Dark mode

--- src/js/layout.js
+++ src/js/layout.js
@@ -1,31 +1,37 @@
 import React from "react";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import ScrollToTop from "./component/scrollToTop";
 
-// import ScrollToTop from "./component/scrollToTop";
-
+import { Home } from "./views/home";
+import { Demo } from "./views/demo";
+import ContactDetails from "./views/ContactDetails";
 import injectContext from "./store/appContext";
 
-import { Contacts } from "./views/Contacts.js";
-import { AddContact } from "./views/AddContact.js";
-import { EditContact } from "./views/EditContact.js";
+import { Navbar } from "./component/navbar";
+import { Footer } from "./component/footer";
 
-export const Layout = () => {
-	return (
-		<div>
-			<BrowserRouter>
-				<div>
-					<Switch>
-						<Route exact path="/index.html" component={Contacts} />
-						<Route exact path="/" component={Contacts} />
-						<Route exact path="/contacts" component={Contacts} />
-						<Route exact path="/add" component={AddContact} />
-						<Route exact path="/edit/:id" component={EditContact} />
-						<Route render={() => <h1 className="notfound">Not found!</h1>} />
-					</Switch>
-				</div>
-			</BrowserRouter>
-		</div>
-	);
+//create your first component
+const Layout = () => {
+  //the basename is used when your project is published in a subdirectory and not in the root of the domain
+  // you can set the basename on the .env file located at the root of this project, E.g: BASENAME=/react-hello-webapp/
+  const basename = process.env.BASENAME || "";
+
+  return (
+    <div>
+      <BrowserRouter basename={basename}>
+        <ScrollToTop>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/demo" element={<Demo />} />
+            <Route path="/contact/:id" element={<ContactDetails />} />
+            <Route path="*" element={<h1>Not found!</h1>} />
+          </Routes>
+          <Footer />
+        </ScrollToTop>
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default injectContext(Layout);
--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -1,96 +1,71 @@
 const getState = ({ getStore, getActions, setStore }) => {
 	return {
-		store: {
-			//Your data structures, A.K.A Entities
-			contacts: [],
-			newContact: {},
-			contactToEdit: {}
+	  store: {
+		contacts: [],
+	  },
+	  actions: {
+		getContacts: async () => {
+		  const response = await fetch(
+			"https://playground.4geeks.com/apis/fake/contact/agenda/agenda_rochelle"
+		  );
+		  const data = await response.json();
+		  setStore({ contacts: data });
 		},
-		actions: {
-			//(Arrow) Functions that update the Store
-			// Remember to use the scope: scope.state.store & scope.setState()
-			getContacts: () => {
-				fetch("https://playground.4geeks.com/apis/fake/contact/", {
-					method: "GET"
-				})
-					.then(response => response.json())
-					//.then(data => console.log(data))
-					.then(data => setStore({ contacts: data }))
-					.catch(error => console.log(error));
-			},
-			// get one contact
-			getOneContact: (id, name, address, phone, email) => {
-				console.log(`passing specific contact information for ${id}`);
-				setStore({
-					contactToEdit: {
-						id: id,
-						agenda_slug: "agenda_rochelle",
-						full_name: name,
-						email: email,
-						phone: phone,
-						address: address
-					}
-				});
-			},
-
-			// getOneContact: id => {
-			// 	console.log(`getting specific contact information for ${id}`);
-			// 	fetch(`https://playground.4geeks.com/apis/fake/contact/${id}`, {
-			// 		method: "GET"
-			// 	})
-			// 		.then(response => response.json())
-			// 		//.then(data => console.log(data))
-			// 		.then(data => setStore({ contactToEdit: data }))
-			// 		.catch(error => console.log(error));
-			// 	console.log(getStore().contactToEdit);
-			// },
-			// edit one contact
-
-			editOneContact: (id, valores) => {
-				fetch(`https://playground.4geeks.com/apis/fake/contact/${id}`, {
-					method: "PUT",
-					headers: {
-						"Content-Type": "application/json"
-					},
-					body: JSON.stringify(valores)
-				})
-					.then(response => response.json())
-					//.then(data => console.log(data))
-					.then(data => setStore({ contacts: data }))
-					.catch(error => console.log(error));
-			},
-
-			createContacts: valores => {
-				fetch("https://playground.4geeks.com/apis/fake/contact/", {
-					method: "POST",
-					headers: {
-						"Content-Type": "application/json"
-					},
-					body: JSON.stringify(valores)
-				})
-					.then(response => response.json())
-					//.then(data => console.log(data))
-					.then(data => setStore({ contacts: data }))
-					.catch(error => console.log(error));
-			},
-			//Delete
-			deleteContacts: id => {
-				fetch(`https://playground.4geeks.com/apis/fake/contact/${id}`, {
-					method: "DELETE"
-				})
-					.then(response => {
-						console.log(response);
-						if (response.ok === true) {
-							getActions().getContacts();
-						}
-						return response.json();
-					})
-					//.then(data => console.log(data))
-					//.then(data => setStore({ contacts: data }))
-					.catch(error => console.log(error));
+		addContact: async (name, phone, email, address) => {
+		  const store = getStore();
+		  const response = await fetch(
+			"https://playground.4geeks.com/apis/fake/contact/",
+			{
+			  method: "POST",
+			  headers: { "Content-Type": "application/json" },
+			  body: JSON.stringify({
+				full_name: name,
+				phone: phone,
+				email: email,
+				address: address,
+				agenda_slug: "agenda_rochelle",
+			  }),
 			}
-		}
+		  );
+		  const data = await response.json();
+		  setStore({ contacts: [...store.contacts, data] });
+		},
+		deleteContact: async (id) => {
+		  const store = getStore();
+		  const response = await fetch(
+			"https://playground.4geeks.com/apis/fake/contact/" + id,
+			{
+			  method: "DELETE",
+			  headers: { "Content-Type": "application/json" },
+			}
+		  );
+		  const data = await response.json();
+		  setStore({
+			contacts: store.contacts.filter((contact) => contact.id !== id),
+		  });
+		},
+		editContact: async (id, name, phone, email, address) => {
+		  const store = getStore();
+		  const response = await fetch(
+			"https://playground.4geeks.com/apis/fake/contact/" + id,
+			{
+			  method: "PUT",
+			  headers: { "Content-Type": "application/json" },
+			  body: JSON.stringify({
+				full_name: name,
+				phone: phone,
+				email: email,
+				address: address,
+				agenda_slug: "agenda_rochelle",
+			  }),
+			}
+		  );
+		  const data = await response.json();
+		  setStore({ contacts: [...store.contacts, data] });
+		},
+	  },
 	};
-};
-
-export default getState;
+  };
+  
+  export default getState;
+  
